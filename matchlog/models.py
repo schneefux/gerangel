44b74007108fca3596ebb9b29808cebe7ae801cd
schneefux <schneefux+commit@schneefux.xyz>
2019-09-30 12:19:52 +0200

    Add player creation signal handler

--- matchlog/models.py
+++ matchlog/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 from trueskill import TrueSkill, Rating
 
 
@@ -13,6 +15,11 @@ class Rating(models.Model):
 
 class Player(Rating):
   user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+@receiver(post_save, sender=User)
+def create_user_player(sender, instance, created, **kwargs):
+    if created:
+        Player.objects.create(user=instance)
 
 
 class Match(models.Model):
